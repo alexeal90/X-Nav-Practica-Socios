057373933db9779ec01d16e143b5038b9457426c
alexeal90 <alexeal90@gmail.com>
2016-04-24 15:52:32 +0200

    prueba

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ $(document).ready(function(){
 
 
 
-/*CORRESPONDIENTE A MOSTRAR Y OCULTAR UPDATE.JSON*/
 
+/*CORRESPONDIENTE A MOSTRAR Y OCULTAR UPDATE.JSON*/
+  $(function() {
     var show = false;
     //var cargado = false;
     var num_usuarios = 0;
@@ -93,7 +94,7 @@ $(document).ready(function(){
         $("#newMsgs").append("Ocultar");
       }
     });
-
+});
 /*CORRESPONDIENTE A LA PARTE DE CONVERSACIONES*/
   	$( ".draggable" ).draggable({stack: "#droppable"});
   	$( "#draggable1" ).resizable({
